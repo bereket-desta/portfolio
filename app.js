195420e2e3a3b882f4e76ed1ff84e291dfb86533
Bereket51 <35872206+bereket-desta@users.noreply.github.com>
2024-05-02 13:40:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function sendMail(){
+  let parms = {
+    name: document.getElementById("name").value
+    email: document.getElementById("email").value
+    number: document.getElementById("number").value
+    message: document.getElementById("message").value
+  }
+emailjs.send("service_g5o8oik", "template_rkvofpt", parms).then(alert("Email sent!"))
+}
