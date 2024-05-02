04c2c0d457e90ee981bcc2a2082d995a6a2cf950
Bereket51 <35872206+bereket-desta@users.noreply.github.com>
2024-05-02 14:47:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function sendMail(){
   let parms= {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
-    number: document.getElementById("number").value,
+    subject: document.getElementById("subject").value,
     message: document.getElementById("message").value
   };
 emailjs.send("service_g5o8oik", "template_rkvofpt", parms).then(alert("Email sent!"));
